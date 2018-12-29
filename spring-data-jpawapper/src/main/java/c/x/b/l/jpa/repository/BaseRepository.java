a8486f527d45610cdbf7b4b5f3cf8234759bc1f5
LiuBingXu18 <1274604226@qq.com>
2018-12-29 20:17:19 +0800

    1

---
+++ spring-data-jpawapper/src/main/java/c/x/b/l/jpa/repository/BaseRepository.java
@@ -0,0 +1,66 @@
+package c.x.b.l.jpa.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.NoRepositoryBean;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * @Author:LiuBingXu
+ * @Description: 自定义Repository接口
+ * @Date: 2018/7/22.
+ * @Modified by
+ */
+@NoRepositoryBean
+public interface BaseRepository<T, ID extends Serializable> extends JpaRepository<T, ID>
+        , JpaSpecificationExecutor<T> {
+
+    boolean support(String modelType);
+
+    List<T> listByHql(String hql);
+
+    List<T> listPageByHql(String hql, int page, int size);
+
+    List<T> listPageByHql(String hql, int page, int size, List<Object> params);
+
+    List<T> listPageByHql(String hql, int page, int size, Object[] params);
+
+    List<T> listPageBySql(String sql, int page, int size);
+
+    List<T> listPageBySql(String sql, int page, int size, List<Object> params);
+
+    List<T> listPageBySql(String sql, int page, int size, Object[] params);
+
+    T findBySql(String sql, Object[] params);
+
+    T findBySql(String sql, List<Object> params);
+
+    List<T> findListBySql(String sql, Object[] params);
+
+    List<T> findListBySql(String sql, List<Object> params);
+
+    List<Object[]> getListBySql(String sql, Object[] params);
+
+    List<Object[]> getListBySql(String sql, List<Object> params);
+
+    Page<T> pageList(Pageable pageable);
+
+    Page<T> pageList(Pageable pageable, Specification specifications);
+
+    int executeUpdateBySql(String sql);
+
+    int executeUpdateBySql(String sql, List<Object> params);
+
+    int executeUpdateBySql(String sql, Object[] params);
+
+    int executeUpdateByHql(String hql);
+
+    int executeUpdateByHql(String hql, List<Object> params);
+
+    int executeUpdateByHql(String hql, Object[] params);
+}
---
+++ spring-data-jpawapper/src/main/java/c/x/b/l/jpa/repository/BaseRepositoryImpl.java
@@ -0,0 +1,204 @@
+package c.x.b.l.jpa.repository;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.transaction.Transactional;
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @Author:LiuBingXu
+ * @Description: jpa封装实现类
+ * @Date: 2018/7/28.
+ * @Modified by
+ */
+public class BaseRepositoryImpl<T, ID extends Serializable> extends SimpleJpaRepository<T, ID>
+        implements BaseRepository<T, ID> {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(BaseRepositoryImpl.class);
+
+    private EntityManager entityManager;
+    private final Class<T> domainClass;
+
+    public BaseRepositoryImpl(Class<T> domainClass, EntityManager em) {
+        super(domainClass, em);
+        this.domainClass = domainClass;
+        this.entityManager = em;
+    }
+
+    @Override
+    public List<T> listByHql(String hql) {
+        return entityManager.createQuery(hql).getResultList();
+    }
+
+    @Override
+    public List<T> listPageByHql(String hql, int page, int size) {
+        return entityManager.createQuery(hql)
+                .setFirstResult(page)
+                .setMaxResults(size)
+                .getResultList();
+    }
+
+    @Override
+    public List<T> listPageByHql(String hql, int page, int size, List<Object> params) {
+        Query query = entityManager.createQuery(hql);
+        for (Object parm : params) {
+            query.setParameter(params.indexOf(parm) + 1, parm);
+            LOGGER.info("binding parms index : " + params.indexOf(parm) + " value : " + parm);
+        }
+        return query.setFirstResult(page)
+                .setMaxResults(size)
+                .getResultList();
+    }
+
+    @Override
+    public List<T> listPageByHql(String hql, int page, int size, Object[] params) {
+        return this.listPageByHql(hql, page, size, Arrays.asList(params));
+    }
+
+    @Override
+    public List<T> listPageBySql(String sql, int page, int size) {
+        return entityManager.createNativeQuery(sql)
+                .setFirstResult(page)
+                .setMaxResults(size)
+                .getResultList();
+    }
+
+    @Override
+    public List<T> listPageBySql(String sql, int page, int size, List<Object> params) {
+        Query query = entityManager.createNativeQuery(sql);
+        for (Object parm : params) {
+            query.setParameter(params.indexOf(parm) + 1, parm);
+            LOGGER.info("binding parms index : " + params.indexOf(parm) + " value : " + parm);
+        }
+        return query.setFirstResult(page)
+                .setMaxResults(size)
+                .getResultList();
+    }
+
+    @Override
+    public List<T> listPageBySql(String sql, int page, int size, Object[] params) {
+        return this.listPageBySql(sql, page, size, Arrays.asList(params));
+    }
+
+    @Override
+    public T findBySql(String sql, Object[] obj) {
+        return this.findBySql(sql, Arrays.asList(obj));
+    }
+
+    @Override
+    public T findBySql(String sql, List<Object> params) {
+        Query nativeQuery = entityManager.createNativeQuery(sql);
+        for (Object parm : params) {
+            nativeQuery.setParameter(params.indexOf(parm) + 1, parm);
+            LOGGER.info("binding parms index : " + params.indexOf(parm) + " value : " + parm);
+        }
+        return (T) nativeQuery.getSingleResult();
+    }
+
+    @Override
+    public List<T> findListBySql(String sql, List<Object> params) {
+        Query nativeQuery = entityManager.createNativeQuery(sql);
+        for (Object parm : params) {
+            nativeQuery.setParameter(params.indexOf(parm) + 1, parm);
+        }
+        return nativeQuery.getResultList();
+    }
+
+    @Override
+    public List<T> findListBySql(String sql, Object[] params) {
+        return this.findListBySql(sql, Arrays.asList(params));
+    }
+
+    @Override
+    public List<Object[]> getListBySql(String sql, Object[] params) {
+        return this.getListBySql(sql, Arrays.asList(params));
+    }
+
+    @Override
+    public List<Object[]> getListBySql(String sql, List<Object> params) {
+        Query nativeQuery = entityManager.createNativeQuery(sql);
+        for (Object parm : params) {
+            nativeQuery.setParameter(params.indexOf(parm) + 1, parm);
+            LOGGER.info("binding parms index : " + params.indexOf(parm) + " value : " + parm);
+        }
+        return nativeQuery.getResultList();
+    }
+
+    @Override
+    public Page<T> pageList(Pageable pageable) {
+        return this.findAll(pageable);
+    }
+
+    @Override
+    public Page<T> pageList(Pageable pageable, Specification specifications) {
+        return this.findAll(specifications, pageable);
+    }
+
+    @Override
+    @Transactional
+    @Modifying
+    public int executeUpdateBySql(String sql) {
+        return entityManager.createNativeQuery(sql).executeUpdate();
+    }
+
+    @Override
+    @Transactional
+    @Modifying
+    public int executeUpdateBySql(String sql, List<Object> params) {
+        Query nativeQuery = entityManager.createNativeQuery(sql);
+        for (Object parm : params) {
+            nativeQuery.setParameter(params.indexOf(parm) + 1, parm);
+            LOGGER.info("binding parms index : " + params.indexOf(parm) + " value : " + parm);
+        }
+        return nativeQuery.executeUpdate();
+    }
+
+    @Override
+    @Transactional
+    @Modifying
+    public int executeUpdateBySql(String sql, Object[] params) {
+        return this.executeUpdateBySql(sql, Arrays.asList(params));
+    }
+
+    @Override
+    @Transactional
+    @Modifying
+    public int executeUpdateByHql(String hql) {
+        return entityManager.createQuery(hql).executeUpdate();
+    }
+
+    @Override
+    @Transactional
+    @Modifying
+    public int executeUpdateByHql(String hql, List<Object> params) {
+        Query query = entityManager.createQuery(hql);
+        for (Object parm : params) {
+            query.setParameter(params.indexOf(parm) + 1, parm);
+            LOGGER.info("binding parms index : " + params.indexOf(parm) + " value : " + parm);
+        }
+        return query.executeUpdate();
+    }
+
+    @Override
+    @Transactional
+    @Modifying
+    public int executeUpdateByHql(String hql, Object[] params) {
+        return this.executeUpdateByHql(hql, Arrays.asList(params));
+    }
+
+
+    @Override
+    public boolean support(String modelType) {
+        return domainClass.getName().equals(modelType);
+    }
+}
